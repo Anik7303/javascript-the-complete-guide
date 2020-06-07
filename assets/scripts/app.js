44c6581cbb552625815a16c45b961cb780bf5443
Anik7303 <anik7303@gmail.com>
2020-06-08 00:43:00 +0600

    finishing section 11

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,16 @@
+/*
+class Person {
+    name = 'Anik';
+    constructor() {
+        this.age = 30;
+    }
+
+    greet() {
+        console.log(`Hi, ${this.name} of age ${this.age}`);
+    }
+}
+*/
+
 function Person() {
     this.name = 'Anik';
     this.age = 23;
@@ -6,10 +19,56 @@ function Person() {
     };
 }
 
+// Person.prototype.printAge = function () {
+//     console.log(this.age);
+// };
+
+Object.setPrototypeOf(Person, {
+    ...Object.getPrototypeOf(Person),
+    printAge() {
+        console.log(this.age);
+    },
+});
+
+const course = {
+    title: 'JavaScript - The Complete Guide',
+    rating: 4.5,
+};
+
 class App {
+    constructor() {}
     static main() {
-        const person = new Person();
-        person.greet();
+        // const person = new Person();
+        // person.greet();
+        // console.log(person.__proto__);
+        // console.dir(Object);
+        // console.log(person);
+        // console.dir(Person);
+
+        Object.setPrototypeOf(course, {
+            ...Object.getPrototypeOf(course),
+            printRating() {
+                console.log(`${this.rating}/5`);
+            },
+        });
+
+        const student = Object.create(
+            {
+                printProgress: function () {
+                    console.log(this.progress);
+                },
+            },
+            {
+                name: {
+                    configurable: false,
+                    enumerable: true,
+                    value: 'Anik',
+                    writable: false,
+                },
+            }
+        );
+        console.log(course);
+        console.log(student);
     }
 }
 
